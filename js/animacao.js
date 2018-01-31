b2006641469e5de72fd55c388b439f58c59db3f9
gustavolopesselin <gustavolopesselin@outlook.com>
2018-01-31 21:15:38 -0200

    Site Gustavo Lopes Selin

---
+++ js/animacao.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+	$(".animar-interno1").addClass("animated fadeInLeft");
+}, 2200);
+setTimeout(function() {
+	$(".animar-interno2").addClass("animated fadeInLeft");
+}, 2500);
+setTimeout(function() {
+	$(".animar-interno3").addClass("animated fadeInLeft");
+}, 2800);
